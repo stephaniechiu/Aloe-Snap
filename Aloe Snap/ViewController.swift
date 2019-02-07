1d8e2ef042c392cbd6784e9ba35538ae5cc68317
Stephanie Chiu <stephaniechiu@outlook.com>
2019-02-07 14:52:13 -0800

    Initial Commit

---
+++ Aloe Snap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aloe Snap
+//
+//  Created by Stephanie on 2/7/19.
+//  Copyright © 2019 Stephanie Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
